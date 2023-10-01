152ab06d298127a4aa1bef9a3d20bba6965ebc11
wendi <z5335959@ad.unsw.edu.au>
2023-10-01 16:41:35 +1100

    modified router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  // useParams,
+  // Outlet
+} from 'react-router-dom'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 
 // pages = signup, signin, dashboard
 
@@ -26,30 +41,53 @@ function AboutUs(){
       )
 }
 
-function App() {
-      const [page, setPage] = React.useState('aboutus')
 
+const SignNav = () => {
+      const url = window.location.href.split('/')
+      const [currentURL, setCurrentURL] = React.useState('/' + url[url.length - 1])
+    
       return (
-      <>
-      <header>
-      <nav>
-            <a href='#' onClick={() => setPage('signup')}>Sign Up</a> 
-            |
-            <a href='#' onClick={() => setPage('signin')}>Sign In</a>
-      </nav>
-            <hr />
-      </header>
-      <main>
-      {page === 'aboutus'
-          ?(<AboutUs/>)
-          :page === 'signup'
-      ? (<SignUp/>)
-      : page === 'signin' 
-      ? (<SignIn/>) 
-      : (<DashBoard/>)}
-      </main>
-      </>
+        <>
+        <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link to='/' style={{ color: 'white', textDecoration: 'none' }}>
+                  BigBrain
+                </Link>
+              </Typography>
+              {(currentURL !== '/play') &&
+                <Button onClick={() => { setCurrentURL('/play') }} color="inherit" ><Link to="/play" style={{ color: 'white', textDecoration: 'none' }}>Join a game</Link></Button>
+              }
+              {(currentURL !== '/login') &&
+                <Button color="inherit" onClick={() => { setCurrentURL('/login') }}><Link to="/login" style={{ color: 'white', textDecoration: 'none' }}>Login</Link></Button>
+              }
+              {(currentURL !== '/register') &&
+                <Button color="inherit" onClick={() => { setCurrentURL('/register') }}><Link to="/register" style={{ color: 'white', textDecoration: 'none' }}>Register</Link></Button>
+              }
+            </Toolbar>
+          </AppBar>
+        </Box>
+        </div>
+        </>
       );
+    }
+
+
+function App() {
+      return (
+            <BrowserRouter>
+              <SignNav />
+              <Routes>
+                <Route path="/" element={<SignUp />} />
+                {/* <Route path="/login" element={<SignIn onSuccess={manageTokenSet} />} /> */}
+                {/* <Route path="/register" element={<SignUp onSuccess={manageTokenSet} />} /> */}
+                {/* <Route path="/play/" element={<EnterSessionPage />} /> */}
+                {/* <Route path="/play/:playerId" element={<PlayGamePage />} /> */}
+              </Routes>
+            </BrowserRouter>
+          );
 }
 
 export default App;
