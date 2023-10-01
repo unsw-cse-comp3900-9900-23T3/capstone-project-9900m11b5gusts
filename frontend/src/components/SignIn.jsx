5aab0d388bc9146484cc83522f80cfcec44d2d5a
syukay <syukay00789@gmail.com>
2023-10-01 13:00:15 +1100

    Update SignIn.jsx

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 function SignIn(){
       const [email, setEmail] = React.useState('')
       const [password, setPassword] = React.useState('')
+      const [page,setPage] = React.useState('aboutus')
+
       async function login(){
             console.log('SignIn:', email, password);
             const response = fetch('http://127.0.0.1:5000/login', {
@@ -23,6 +25,7 @@ function SignIn(){
       Email:<input value={email} onChange={(e)=>setEmail(e.target.value)}/><br />
       Password: <input value={password} onChange={(e)=>setPassword(e.target.value)}/><br />
       <button onClick={login}>Sign in</button>
+      <a href='#' onClick={() => setPage('resetRequest')}>Forgot Password?</a>
       </>
       )
 }
